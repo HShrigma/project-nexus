c8f17aeb085d479de06170605024c79f8117f68c
HShrigma <hristostantchev@abv.bg>
2026-02-23 13:01:29 +0200

    feat(product): implement product counter placeholder

--- Project Nexus/src/App.jsx
+++ Project Nexus/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
 import Header from './components/header/Header'
+import ProductCounter from './components/products/ProductCounter';
 
 function App() {
   return (
     <>
           <Header />
+          <ProductCounter/>
           <h1 className='ml-20 bg-red-200 text-red-800'> Hello World</h1>
     </>
   )
---
+++ Project Nexus/src/components/products/ProductCounter.jsx
@@ -0,0 +1,9 @@
+export const ProductCounter = () => {
+    return (
+        <div>
+            X out of Y items displayed
+        </div>
+    )
+}
+
+export default ProductCounter;

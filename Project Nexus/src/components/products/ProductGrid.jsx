b9052e6a76903e63ef6d45e741d279c467144a61
HShrigma <hristostantchev@abv.bg>
2026-02-23 13:09:24 +0200

    feat(product): add product placeholder

--- Project Nexus/src/components/products/ProductGrid.jsx
+++ Project Nexus/src/components/products/ProductGrid.jsx
@@ -1,9 +1,11 @@
+import { Product } from "./Product";
+
 export const ProductGrid = () => {
     const tempProducts = [{ id: 1, name: "Bag 1" }, { id: 2, name: "Bag 2" }, { id: 3, name: "Bag 3" }];
     return (
         <div>
             This is a product grid
-            {tempProducts.map(prod => <div>{prod.name}</div>)}
+            {tempProducts.map(prod => <Product key={prod.id} name={prod.name}/>)}
         </div>
     )
 }

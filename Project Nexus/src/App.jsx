e5248e79916afde6602b8ab6d0436a738dcee56b
HShrigma <hristostantchev@abv.bg>
2026-02-23 12:41:39 +0200

    chore(setup): implement vue project - create vite app - include tailwindcss

---
+++ Project Nexus/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <h1 className='ml-20 bg-red-200 text-red-800'> Hello World</h1>
+    </>
+  )
+}
+
+export default App

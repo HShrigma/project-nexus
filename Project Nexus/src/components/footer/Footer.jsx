e96a59cee79739f436d5e15ad58feaa06d220adb
HShrigma <hristostantchev@abv.bg>
2026-02-25 15:30:18 +0200

    feat(footer): style footer

--- Project Nexus/src/components/footer/Footer.jsx
+++ Project Nexus/src/components/footer/Footer.jsx
@@ -1,9 +1,25 @@
 export const Footer = () => {
-    return(
-        <footer>
-            <a> Terms & Conditions </a>
-            <a> Privacy Policy </a>
-            <a> Contact Us </a>
+    return (
+        <footer className="py-6 px-4 my-3">
+            <div className="max-w-7xl mx-auto flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-4 border-t-1 border-neutral-400">
+                {/* Links */}
+                <div className="flex flex-col sm:flex-row gap-4 text-sm">
+                    <a href="#" className="hover:underline">
+                        Terms & Conditions
+                    </a>
+                    <a href="#" className="hover:underline">
+                        Privacy Policy
+                    </a>
+                    <a href="#" className="hover:underline">
+                        Contact Us
+                    </a>
+                </div>
+
+                {/* Copyright */}
+                <div className="text-sm text-gray-400 mt-2 sm:mt-0">
+                    © {new Date().getFullYear()}
+                </div>
+            </div>
         </footer>
-    )
-}
+    );
+};
